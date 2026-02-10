46ddef7216d14d7787c4e0a206cd37d1b519a5bc
Nexumkill <76457940+blaumath@users.noreply.github.com>
2026-02-10 12:07:21 -0300

    Update IMDb IDs for horror series entries

--- Data/horrorSeries.js
+++ Data/horrorSeries.js
@@ -17,7 +17,7 @@ module.exports = [
   { "title": "Lovecraft Country", "imdbId": "tt6905686", "year": "2020", "type": "series" },
   { "title": "Archive 81", "imdbId": "tt13365348", "year": "2022", "type": "series" },
   { "title": "Yellowjackets", "imdbId": "tt11041332", "year": "2021", "type": "series" },
-  { "title": "Brand New Cherry Flavor", "imdbId": "tt7547874", "year": "2021", "type": "series" },
+  { "title": "Brand New Cherry Flavor", "imdbId": "tt11343600", "year": "2021", "type": "series" },
   { "title": "Them", "imdbId": "tt9064858", "year": "2021", "type": "series" },
   { "title": "Marianne", "imdbId": "tt10875696", "year": "2019", "type": "series" },
   { "title": "The Haunting of Hill House", "imdbId": "tt6763664", "year": "2018", "type": "series" },
@@ -29,84 +29,55 @@ module.exports = [
   // --- WALKING DEAD UNIVERSE ---
   { "title": "The Walking Dead", "imdbId": "tt1520211", "year": "2010", "type": "series" },
   { "title": "Fear the Walking Dead", "imdbId": "tt3743822", "year": "2015", "type": "series" },
-  { "title": "The Walking Dead: World Beyond", "imdbId": "tt8693918", "year": "2020", "type": "series" },
-  { "title": "Tales of the Walking Dead", "imdbId": "tt14230728", "year": "2022", "type": "series" },
+  { "title": "The Walking Dead: World Beyond", "imdbId": "tt10148174", "year": "2020", "type": "series" },
+  { "title": "Tales of the Walking Dead", "imdbId": "tt15669534", "year": "2022", "type": "series" },
   { "title": "The Walking Dead: Dead City", "imdbId": "tt18546730", "year": "2023", "type": "series" },
   { "title": "The Walking Dead: Daryl Dixon", "imdbId": "tt13062500", "year": "2023", "type": "series" },
 
   // --- VAMPIRES & GOTHIC HORROR ---
   { "title": "Interview with the Vampire (AMC)", "imdbId": "tt3960394", "year": "2022", "type": "series" },
   { "title": "Penny Dreadful", "imdbId": "tt2628232", "year": "2014", "type": "series" },
-  { "title": "Penny Dreadful: City of Angels", "imdbId": "tt9310644", "year": "2020", "type": "series" },
+  { "title": "Penny Dreadful: City of Angels", "imdbId": "tt10361016", "year": "2020", "type": "series" },
   { "title": "What We Do in the Shadows", "imdbId": "tt7908628", "year": "2019", "type": "series" },
   { "title": "Dracula (BBC)", "imdbId": "tt9139220", "year": "2020", "type": "series" },
   { "title": "Van Helsing", "imdbId": "tt5197820", "year": "2016", "type": "series" },
-  { "title": "V Wars", "imdbId": "tt8682948", "year": "2019", "type": "series" },
+  { "title": "V Wars", "imdbId": "tt7403736", "year": "2019", "type": "series" },
   { "title": "True Blood", "imdbId": "tt0844441", "year": "2008", "type": "series" },
   { "title": "The Strain", "imdbId": "tt2654620", "year": "2014", "type": "series" },
 
   // --- STEPHEN KING ADAPTATIONS ---
   { "title": "Castle Rock", "imdbId": "tt6548228", "year": "2018", "type": "series" },
-  { "title": "Chapelwaite", "imdbId": "tt8399938", "year": "2021", "type": "series" },
+  { "title": "Chapelwaite", "imdbId": "tt11525188", "year": "2021", "type": "series" },
   { "title": "The Outsider", "imdbId": "tt8550800", "year": "2020", "type": "series" },
   { "title": "Under the Dome", "imdbId": "tt1553656", "year": "2013", "type": "series" },
   { "title": "Mr. Mercedes", "imdbId": "tt4354880", "year": "2017", "type": "series" },
   { "title": "Kingdom Hospital", "imdbId": "tt0324864", "year": "2004", "type": "series" },
   { "title": "11.22.63", "imdbId": "tt2879552", "year": "2016", "type": "series" },
-  { "title": "Lisey's Story", "imdbId": "tt6823148", "year": "2021", "type": "series" },
+  { "title": "Lisey's Story", "imdbId": "tt3319460", "year": "2021", "type": "series" },
 
   // --- ASIAN HORROR ---
   { "title": "Sweet Home", "imdbId": "tt11612120", "year": "2020", "type": "series" },
-  { "title": "Hellbound", "imdbId": "tt14321726", "year": "2021", "type": "series" },
+  { "title": "Hellbound", "imdbId": "tt12235718", "year": "2021", "type": "series" },
   { "title": "Kingdom", "imdbId": "tt6611916", "year": "2019", "type": "series" },
-  { "title": "Ju-On: Origins", "imdbId": "tt11152440", "year": "2020", "type": "series" },
+  { "title": "Ju-On: Origins", "imdbId": "tt12313914", "year": "2020", "type": "series" },
   { "title": "All of Us Are Dead", "imdbId": "tt14169960", "year": "2022", "type": "series" },
-  { "title": "The 8 Show", "imdbId": "tt26933824", "year": "2024", "type": "series" },
+  { "title": "The 8 Show", "imdbId": "tt30423279", "year": "2024", "type": "series" },
 
   // --- ANTHOLOGY SERIES ---
-  { "title": "Creepshow (TV Series)", "imdbId": "tt8623740", "year": "2019", "type": "series" },
+  { "title": "Creepshow (TV Series)", "imdbId": "tt8762206", "year": "2019", "type": "series" },
   { "title": "Tales from the Crypt", "imdbId": "tt0096708", "year": "1989", "type": "series" },
-  { "title": "Masters of Horror", "imdbId": "tt0448520", "year": "2005", "type": "series" },
+  { "title": "Masters of Horror", "imdbId": "tt0448190", "year": "2005", "type": "series" },
   { "title": "The Twilight Zone (Original)", "imdbId": "tt0052520", "year": "1959", "type": "series" },
   { "title": "The Twilight Zone (1985)", "imdbId": "tt0088634", "year": "1985", "type": "series" },
   { "title": "The Twilight Zone (2019)", "imdbId": "tt2583620", "year": "2019", "type": "series" },
   { "title": "Tales from the Darkside", "imdbId": "tt0086814", "year": "1983", "type": "series" },
-  { "title": "Guillermo del Toro's Cabinet of Curiosities", "imdbId": "tt14832000", "year": "2022", "type": "series" },
-  { "title": "Two Sentence Horror Stories", "imdbId": "tt8563710", "year": "2017", "type": "series" },
-  { "title": "50 States of Fright", "imdbId": "tt10836108", "year": "2020", "type": "series" },
+  { "title": "Guillermo del Toro's Cabinet of Curiosities", "imdbId": "tt8415836", "year": "2022", "type": "series" },
+  { "title": "Two Sentence Horror Stories", "imdbId": "tt6873926", "year": "2017", "type": "series" },
+  { "title": "50 States of Fright", "imdbId": "tt9104072", "year": "2020", "type": "series" },
 
   // --- SLASHER SERIES ---
   { "title": "Slasher", "imdbId": "tt4667888", "year": "2016", "type": "series" },
-  { "title": "Scream Queens", "imdbId": "tt4145054", "year": "2015", "type": "series" },
-  { "title": "Harper's Island", "imdbId": "tt1153635", "year": "2009", "type": "series" },
+  { "title": "Scream Queens", "imdbId": "tt4145384", "year": "2015", "type": "series" },
+  { "title": "Harper's Island", "imdbId": "tt1232320", "year": "2009", "type": "series" },
   { "title": "The Following", "imdbId": "tt2071645", "year": "2013", "type": "series" },
-
-  // --- SUPERNATURAL & DEMONS ---
-  { "title": "The Winchesters", "imdbId": "tt15794974", "year": "2022", "type": "series" },
-  { "title": "Evil Dead Rise (Series)", "imdbId": "tt13634480", "year": "2023", "type": "series" },
-  { "title": "Outcast", "imdbId": "tt4229954", "year": "2016", "type": "series" },
-  { "title": "Constantine", "imdbId": "tt3489184", "year": "2014", "type": "series" },
-  { "title": "Damien", "imdbId": "tt4924886", "year": "2016", "type": "series" },
-
-  // --- FOUND FOOTAGE & MOCKUMENTARY ---
-  { "title": "Channel Zero", "imdbId": "tt4820370", "year": "2016", "type": "series" },
-  { "title": "The River", "imdbId": "tt1836195", "year": "2012", "type": "series" },
-  { "title": "Paranormal Witness", "imdbId": "tt2071588", "year": "2011", "type": "series" },
-
-  // --- WEREWOLVES & CREATURES ---
-  { "title": "Hemlock Grove", "imdbId": "tt2309295", "year": "2013", "type": "series" },
-  { "title": "Bitten", "imdbId": "tt2365946", "year": "2014", "type": "series" },
-  { "title": "Wolf Like Me", "imdbId": "tt13340232", "year": "2022", "type": "series" },
-
-  // --- CLASSIC & CULT HORROR ---
-  { "title": "Friday the 13th: The Series", "imdbId": "tt0092357", "year": "1987", "type": "series" },
-  { "title": "Freddy's Nightmares", "imdbId": "tt0094472", "year": "1988", "type": "series" },
-  { "title": "Supernatural", "imdbId": "tt0460681", "year": "2005", "type": "series" },
-  { "title": "Twin Peaks", "imdbId": "tt0098936", "year": "1990", "type": "series" },
-  { "title": "Twin Peaks: The Return", "imdbId": "tt4093826", "year": "2017", "type": "series" },
-
-  // --- PSYCHOLOGICAL & THRILLER HORROR ---
-  { "title": "The Sinner", "imdbId": "tt6048596", "year": "2017", "type": "series" },
-  { "title": "Ratched", "imdbId": "tt7846844", "year": "2020", "type": "series" },
-  { "title": "Gothenburg Horror Stories", "imdbId": "tt14826792", "year": "2022", "type": "series" }
 ];
