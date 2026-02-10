17d46bf10d24f63360a0635fa464ff746b959fcf
Nexumkill <76457940+blaumath@users.noreply.github.com>
2026-02-10 11:19:40 -0300

    Update IMDb IDs for Paranormal Activity movies

--- Data/paranormalActivity.js
+++ Data/paranormalActivity.js
@@ -4,6 +4,6 @@ module.exports = [
   { "title": "Paranormal Activity 3", "imdbId": "tt1778304", "year": "2011" },
   { "title": "Paranormal Activity 4", "imdbId": "tt2109184", "year": "2012" },
   { "title": "Paranormal Activity: The Marked Ones", "imdbId": "tt2473682", "year": "2014" },
-  { "title": "Paranormal Activity: The Ghost Dimension", "imdbId": "tt2473664", "year": "2015" },
-  { "title": "Paranormal Activity: Next of Kin", "imdbId": "tt9286706", "year": "2021" }
+  { "title": "Paranormal Activity: The Ghost Dimension", "imdbId": "tt2473510", "year": "2015" },
+  { "title": "Paranormal Activity: Next of Kin", "imdbId": "tt10515988", "year": "2021" }
 ];
