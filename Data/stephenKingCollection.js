44e3ef44a3923eb5106c9314f10abadc5245b287
Nexumkill <76457940+blaumath@users.noreply.github.com>
2026-02-10 11:54:41 -0300

    Fix IMDb IDs and years in Stephen King collection
    Updated IMDb IDs and years for several titles in the Stephen King collection.

--- Data/stephenKingCollection.js
+++ Data/stephenKingCollection.js
@@ -1,6 +1,5 @@
 module.exports = [
   // --- IT UNIVERSE ---
-  { "title": "IT", "imdbId": "tt0099864", "year": "1990", "type": "series" },
   { "title": "IT Chapter One", "imdbId": "tt1396484", "year": "2017" },
   { "title": "IT Chapter Two", "imdbId": "tt7349950", "year": "2019" },
   { "title": "Welcome to Derry (IT Prequel Series)", "imdbId": "tt19244304", "year": "2025", "type": "series" },
@@ -22,22 +21,21 @@ module.exports = [
 
   // --- SALEM'S LOT ---
   { "title": "Salem's Lot (Mini-series)", "imdbId": "tt0079844", "year": "1979", "type": "series" },
-  { "title": "A Return to Salem's Lot", "imdbId": "tt0093858", "year": "1987" },
-  { "title": "Salem's Lot (Remake)", "imdbId": "tt0120800", "year": "2004" },
+  { "title": "A Return to Salem's Lot", "imdbId": "tt0093855", "year": "1987" },
+  { "title": "Salem's Lot (Remake)", "imdbId": "tt0355987", "year": "2004" },
   { "title": "Salem's Lot", "imdbId": "tt10245072", "year": "2024" },
 
   // --- CHILDREN OF THE CORN ---
   { "title": "Children of the Corn", "imdbId": "tt0087050", "year": "1984" },
-  { "title": "Children of the Corn II: The Final Sacrifice", "imdbId": "tt0104047", "year": "1992" },
-  { "title": "Children of the Corn III: Urban Harvest", "imdbId": "tt0112681", "year": "1995" },
-  { "title": "Children of the Corn IV: The Gathering", "imdbId": "tt0115862", "year": "1996" },
-  { "title": "Children of the Corn V: Fields of Terror", "imdbId": "tt0157015", "year": "1998" },
-  { "title": "Children of the Corn 666: Isaac's Return", "imdbId": "tt0180637", "year": "1999" },
-  { "title": "Children of the Corn: Revelation", "imdbId": "tt0284726", "year": "2001" },
-  { "title": "Children of the Corn: Genesis", "imdbId": "tt1550560", "year": "2011" },
-  { "title": "Children of the Corn: Runaway", "imdbId": "tt6155518", "year": "2018" },
+  { "title": "Children of the Corn II: The Final Sacrifice", "imdbId": "tt0106557", "year": "1992" },
+  { "title": "Children of the Corn III: Urban Harvest", "imdbId": "tt0109415", "year": "1995" },
+  { "title": "Children of the Corn IV: The Gathering", "imdbId": "tt0115885", "year": "1996" },
+  { "title": "Children of the Corn V: Fields of Terror", "imdbId": "tt0150111", "year": "1998" },
+  { "title": "Children of the Corn 666: Isaac's Return", "imdbId": "tt0177625", "year": "1999" },
+  { "title": "Children of the Corn: Revelation", "imdbId": "tt0279730", "year": "2001" },
+  { "title": "Children of the Corn: Genesis", "imdbId": "tt1745672", "year": "2011" },
+  { "title": "Children of the Corn: Runaway", "imdbId": "tt5565634", "year": "2018" },
   { "title": "Children of the Corn (Remake)", "imdbId": "tt12311866", "year": "2020" },
-  { "title": "Children of the Corn (Remake)", "imdbId": "tt11976570", "year": "2023" },
 
   // --- THE STAND ---
   { "title": "The Stand (Mini-series)", "imdbId": "tt0108941", "year": "1994", "type": "series" },
@@ -50,10 +48,10 @@ module.exports = [
   { "title": "Christine", "imdbId": "tt0085333", "year": "1983" },
   { "title": "Creepshow", "imdbId": "tt0083767", "year": "1982" },
   { "title": "Creepshow 2", "imdbId": "tt0092796", "year": "1987" },
-  { "title": "Creepshow 3", "imdbId": "tt0473405", "year": "2006" },
+  { "title": "Creepshow 3", "imdbId": "tt0478125", "year": "2006" },
   { "title": "The Dead Zone", "imdbId": "tt0085407", "year": "1983" },
-  { "title": "Firestarter", "imdbId": "tt0082374", "year": "1984" },
-  { "title": "Firestarter (Remake)", "imdbId": "tt8760708", "year": "2022" },
+  { "title": "Firestarter", "imdbId": "tt0087262", "year": "1984" },
+  { "title": "Firestarter (Remake)", "imdbId": "tt1798632", "year": "2022" },
   { "title": "Cat's Eye", "imdbId": "tt0088889", "year": "1985" },
   { "title": "Silver Bullet", "imdbId": "tt0090021", "year": "1985" },
   { "title": "Maximum Overdrive", "imdbId": "tt0091499", "year": "1986" },
@@ -62,29 +60,29 @@ module.exports = [
   { "title": "The Green Mile", "imdbId": "tt0120689", "year": "1999" },
   { "title": "Dreamcatcher", "imdbId": "tt0285531", "year": "2003" },
   { "title": "Secret Window", "imdbId": "tt0363988", "year": "2004" },
-  { "title": "Riding the Bullet", "imdbId": "tt0330190", "year": "2004" },
+  { "title": "Riding the Bullet", "imdbId": "tt0355954", "year": "2004" },
   { "title": "1408", "imdbId": "tt0450385", "year": "2007" },
-  { "title": "Dolan's Cadillac", "imdbId": "tt0893382", "year": "2009" },
-  { "title": "A Good Marriage", "imdbId": "tt2170514", "year": "2014" },
+  { "title": "Dolan's Cadillac", "imdbId": "tt0963965", "year": "2009" },
+  { "title": "A Good Marriage", "imdbId": "tt2180994", "year": "2014" },
   { "title": "Cell", "imdbId": "tt0775440", "year": "2016" },
 
   // --- RECENT RELEASES ---
   { "title": "Gerald's Game", "imdbId": "tt3748172", "year": "2017" },
   { "title": "1922", "imdbId": "tt6214928", "year": "2017" },
   { "title": "In the Tall Grass", "imdbId": "tt4687108", "year": "2019" },
-  { "title": "Mr. Harrigan's Phone", "imdbId": "tt11242218", "year": "2022" },
+  { "title": "Mr. Harrigan's Phone", "imdbId": "tt12908110", "year": "2022" },
   { "title": "The Boogeyman", "imdbId": "tt3427252", "year": "2023" },
 
   // --- SERIES ---
-  { "title": "The Dead Zone (TV Series)", "imdbId": "tt0281249", "year": "2002", "type": "series" },
+  { "title": "The Dead Zone (TV Series)", "imdbId": "tt0281432", "year": "2002", "type": "series" },
   { "title": "Kingdom Hospital", "imdbId": "tt0324864", "year": "2004", "type": "series" },
   { "title": "Under the Dome", "imdbId": "tt1553656", "year": "2013", "type": "series" },
   { "title": "11.22.63", "imdbId": "tt2879552", "year": "2016", "type": "series" },
   { "title": "Mr. Mercedes", "imdbId": "tt4354880", "year": "2017", "type": "series" },
   { "title": "Castle Rock", "imdbId": "tt6548228", "year": "2018", "type": "series" },
   { "title": "The Outsider", "imdbId": "tt8550800", "year": "2020", "type": "series" },
-  { "title": "Lisey's Story", "imdbId": "tt6823148", "year": "2021", "type": "series" },
-  { "title": "Chapelwaite", "imdbId": "tt8399938", "year": "2021", "type": "series" },
-  { "title": "The Tommyknockers (Mini-series)", "imdbId": "tt0106169", "year": "1993", "type": "series" },
-  { "title": "Creepshow (TV Series)", "imdbId": "tt8623740", "year": "2019", "type": "series" }
+  { "title": "Lisey's Story", "imdbId": "tt3319460", "year": "2021", "type": "series" },
+  { "title": "Chapelwaite", "imdbId": "tt11525188", "year": "2021", "type": "series" },
+  { "title": "The Tommyknockers (Mini-series)", "imdbId": "tt0106156", "year": "1993", "type": "series" },
+  { "title": "Creepshow (TV Series)", "imdbId": "tt8762206", "year": "2019", "type": "series" }
 ];
